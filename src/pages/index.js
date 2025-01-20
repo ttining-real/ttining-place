b05932a6b66365cf3144218b67dc0db8af17c40f
ttining <ttining.real@gmail.com>
2025-01-20 16:14:41 +0900

    [초기화] public / src 폴더 초기화

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-// 포트폴리오 페이지
-export { default as Error } from "./Error/Error";
-export { default as Home } from "./Home/Home";
-export { default as Resume } from "./Resume/Resume";
-export { default as Projects } from "./Projects/Projects";
