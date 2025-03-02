be44c7110f8e6731f6d0603d5c589e451d0c8cd2
ttining <ttining.real@gmail.com>
2025-03-03 08:58:57 +0900

    [수정] Switch 컴포넌트 분리

---
+++ src/components/Switch/Switch.jsx
@@ -0,0 +1,31 @@
+import { bool, func, string } from "prop-types";
+
+import $ from "./Switch.module.scss";
+
+function Switch({ label, labelId, checked, onChange }) {
+  return (
+    <div className={$.switch_checkbox}>
+      <label id={labelId}>
+        <input
+          role='switch'
+          type='checkbox'
+          aria-labelledby={labelId}
+          aria-checked={checked}
+          checked={checked}
+          className={$.checkbox}
+          onChange={onChange}
+        />
+        <span>{label}</span>
+      </label>
+    </div>
+  );
+}
+
+Switch.propTypes = {
+  label: string.isRequired,
+  labelId: string.isRequired,
+  checked: bool.isRequired,
+  onChange: func.isRequired,
+};
+
+export default Switch;
