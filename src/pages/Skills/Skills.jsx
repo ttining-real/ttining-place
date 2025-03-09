34b25a5b619757360f66b676a980f0b4f177c03b
ttining <ttining.real@gmail.com>
2025-03-09 11:22:58 +0900

    [feature] Pages Skills 생성

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,5 +1,26 @@
+import SubTitle from "@/components/SubTitle/SubTitle";
+import { SkillsData } from "@/mockData/Skills";
+
+import S from "./Skills.module.scss";
+
 function Skills() {
-  return <div>Skills Page</div>;
+  return (
+    <main className={S.wrap}>
+      {SkillsData.map((item, index) => (
+        <section key={index}>
+          <SubTitle title={item.title} />
+          <p className='body'>{item.description}</p>
+          <div className={S.chip_set}>
+            {item.chips.map((chip, chipIndex) => (
+              <span key={chipIndex} className={`body ${S.chip}`}>
+                {chip}
+              </span>
+            ))}
+          </div>
+        </section>
+      ))}
+    </main>
+  );
 }
 
 export default Skills;
