9d29fde50d0fa2250a1f36023e4564e066fe06fb
ttining <ttining.real@gmail.com>
2025-03-09 11:05:10 +0900

    [feature] Pages(Career, Experience) 생성

--- src/pages/Career/Career.jsx
+++ src/pages/Career/Career.jsx
@@ -1,5 +1,65 @@
+import SubTitle from "@/components/SubTitle/SubTitle";
+import Timeline from "@/components/Timeline/Timeline";
+import {
+  YEAR_2016,
+  YEAR_2017,
+  YEAR_2018,
+  YEAR_2019,
+  YEAR_2020,
+  YEAR_2021,
+  YEAR_2022,
+  YEAR_2023,
+  YEAR_2024,
+  YEAR_2025,
+} from "@/mockData/Career";
+
+import S from "./Career.module.scss";
+
 function Career() {
-  return <div>Career Page</div>;
+  return (
+    <main className={S.wrap}>
+      <section>
+        <SubTitle title='2025' />
+        <Timeline list={YEAR_2025} />
+      </section>
+      <section>
+        <SubTitle title='2024' />
+        <Timeline list={YEAR_2024} />
+      </section>
+      <section>
+        <SubTitle title='2023' />
+        <Timeline list={YEAR_2023} />
+      </section>
+      <section>
+        <SubTitle title='2022' />
+        <Timeline list={YEAR_2022} />
+      </section>
+      <section>
+        <SubTitle title='2021' />
+        <Timeline list={YEAR_2021} />
+      </section>
+      <section>
+        <SubTitle title='2020' />
+        <Timeline list={YEAR_2020} />
+      </section>
+      <section>
+        <SubTitle title='2019' />
+        <Timeline list={YEAR_2019} />
+      </section>
+      <section>
+        <SubTitle title='2018' />
+        <Timeline list={YEAR_2018} />
+      </section>
+      <section>
+        <SubTitle title='2017' />
+        <Timeline list={YEAR_2017} />
+      </section>
+      <section>
+        <SubTitle title='2016' />
+        <Timeline list={YEAR_2016} />
+      </section>
+    </main>
+  );
 }
 
 export default Career;
--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,5 +1,22 @@
+import SubTitle from "@/components/SubTitle/SubTitle";
+import Timeline from "@/components/Timeline/Timeline";
+import { OtherList, WorkList } from "@/mockData/Experience";
+
+import S from "./Experience.module.scss";
+
 function Experience() {
-  return <div>Experience Page</div>;
+  return (
+    <main className={S.wrap}>
+      <section>
+        <SubTitle title='Work Experience' />
+        <Timeline list={WorkList} />
+      </section>
+      <section>
+        <SubTitle title='Other Experience' />
+        <Timeline list={OtherList} />
+      </section>
+    </main>
+  );
 }
 
 export default Experience;
