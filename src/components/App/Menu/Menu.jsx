759133db0e8df324d2ed531f07b88d2942d9ca4d
ttining <ttining.real@gmail.com>
2025-03-16 11:33:02 +0900

    [modify] Menu 컴포넌트 수정
    - isMenuClicked 기능 전달

--- src/components/App/Menu/Menu.jsx
+++ src/components/App/Menu/Menu.jsx
@@ -1,36 +1,41 @@
-import { bool } from "prop-types";
+import { bool, func } from "prop-types";
 import { Link } from "react-router-dom";
 
 import S from "./Menu.module.scss";
 
-function Menu({ isMenuOpen }) {
+function Menu({ isMenuOpen, isMenuClicked }) {
   const menuClass = `${S.menu} ${isMenuOpen ? "" : S.open}`;
 
   return (
     <nav className={menuClass}>
       <ul>
         <li>
-          <Link to='/about' className='title'>
+          <Link to='/' className='title' onClick={isMenuClicked}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/about' className='title' onClick={isMenuClicked}>
             About
           </Link>
         </li>
         <li>
-          <Link to='/skills' className='title'>
+          <Link to='/skills' className='title' onClick={isMenuClicked}>
             Skills
           </Link>
         </li>
         <li>
-          <Link to='/experience' className='title'>
+          <Link to='/experience' className='title' onClick={isMenuClicked}>
             Experience
           </Link>
         </li>
         <li>
-          <Link to='/career' className='title'>
+          <Link to='/career' className='title' onClick={isMenuClicked}>
             Career
           </Link>
         </li>
         <li>
-          <Link to='/projects' className='title'>
+          <Link to='/projects' className='title' onClick={isMenuClicked}>
             Projects
           </Link>
         </li>
@@ -41,6 +46,7 @@ function Menu({ isMenuOpen }) {
 
 Menu.propTypes = {
   isMenuOpen: bool.isRequired,
+  isMenuClicked: func.isRequired,
 };
 
 export default Menu;
