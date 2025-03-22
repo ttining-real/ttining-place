6dcd827b4e284df555bddaba58062f5a727de482
ttining <ttining.real@gmail.com>
2025-03-22 21:06:21 +0900

    [modify] pages/index.js 수정
    - Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 export { default as About } from "./About/About";
 export { default as Career } from "./Career/Career";
 export { default as Experience } from "./Experience/Experience";
+export { default as Home } from "./Home/Home";
 export { default as Projects } from "./Projects/Projects";
 export { default as Skills } from "./Skills/Skills";
 export { default as Test } from "./Test/Test";
