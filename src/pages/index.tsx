56d6389752ecacc392684cf2dac139b45bce786e
ttining <ttining.real@gmail.com>
2025-04-06 11:38:28 +0900

    [Feature] 홈 화면 레이아웃
    - 임시 레이아웃 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,71 @@
 import localFont from 'next/font/local';
+import Link from 'next/link';
 
-const baseFont = localFont({
-  src: '../fonts/PretendardVariable.woff2',
+const gmarket = localFont({
+  src: [
+    {
+      path: '../fonts/GmarketSansTTFBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/GmarketSansTTFMedium.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/GmarketSansTTFLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+  ],
   display: 'swap',
-  variable: '--baseFont',
-});
-
-const titleFont = localFont({
-  src: '../fonts/GmarketSansTTFBold.ttf',
-  display: 'swap',
-  variable: '--titleFont',
+  variable: '--gmarketFont',
 });
 
 export default function Home() {
   return (
-    <div className={` ${baseFont.className} bg-primary text-2xl`}>
-      <p className="font-black">Pretendard Black</p>
-      <p className="font-extrabold">Pretendard ExtraBold</p>
-      <p className="font-bold">Pretendard Bold</p>
-      <p className="font-semibold">Pretendard SemiBold</p>
-      <p className="titleFont">Pretendard Medium</p>
-      <p className="font-regular">Pretendard Regular</p>
-      <p className="font-light">Pretendard Light</p>
-      <p className="font-extralight">Pretendard ExtraLight</p>
-      <p className="font-thin">Pretendard Thin</p>
-      <p className={`${titleFont.className} title-font`}>
-        GmarketSans Bold for Title
-      </p>
-    </div>
+    <>
+      <section className="bg-gray-10 flex h-[360px] flex-col items-center justify-center gap-6">
+        <h2
+          className={`${gmarket.className} gmarket text-2xl font-bold text-white`}
+        >
+          Welcome! An Jiin Portfolio
+        </h2>
+        <p className="text-base text-white">
+          I worked as a UX/UI designer and web publisher, Recently, I have been
+          studying the front end.
+        </p>
+        <Link
+          href="/about"
+          className="bg-primary rounded-md px-4 py-1.5 text-base text-white"
+        >
+          Go About
+        </Link>
+      </section>
+      <section className="m-auto flex max-w-5xl flex-col gap-6 py-12">
+        <header className="flex flex-col items-center gap-1">
+          <h3 className={`${gmarket.className} gmarket text-2xl font-bold`}>
+            Projects
+          </h3>
+          <p>Front-end Development Project</p>
+        </header>
+        <div className="grid grid-cols-2 gap-6">
+          <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
+          <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
+          <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
+          <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
+        </div>
+      </section>
+      <section className="m-auto flex max-w-5xl flex-col gap-6 py-12">
+        <header className="flex flex-col items-center gap-1">
+          <h3 className={`${gmarket.className} gmarket text-2xl font-bold`}>
+            Experience
+          </h3>
+          <p>Companies and Freelance</p>
+        </header>
+        <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
+      </section>
+    </>
   );
 }
