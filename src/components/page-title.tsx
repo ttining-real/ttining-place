cb4a81aed74e8f83c05af68e5138f9e0feeb0d11
ttining <ttining.real@gmail.com>
2025-04-08 18:43:29 +0900

    [Feature] 페이지별 타이틀 컴포넌트화
    - 샘플 이미지 추가
    - 컴포넌트 생성

---
+++ src/components/page-title.tsx
@@ -0,0 +1,54 @@
+import localFont from 'next/font/local';
+
+type pageTitleProps = {
+  title: string;
+  description: string[];
+};
+
+const gmarket = localFont({
+  src: [
+    {
+      path: '../fonts/GmarketSansTTFBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/GmarketSansTTFMedium.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/GmarketSansTTFLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--gmarketFont',
+});
+
+export default function PageTitle({ title, description }: pageTitleProps) {
+  return (
+    <section className="flex h-[360px] items-center justify-center bg-white bg-[url('/images/sample_about.png')] bg-cover bg-center bg-no-repeat">
+      <div className="m-auto flex max-w-5xl flex-1 flex-col items-center justify-between gap-4 px-6 md:flex-row">
+        <div>
+          <h2
+            className={`${gmarket.className} flex flex-col items-center gap-2 text-3xl font-bold`}
+          >
+            {title}
+          </h2>
+          <hr className="w-[80px] border-2 border-white/50 dark:border-black/50" />
+        </div>
+        {description ? (
+          <div className="text-center font-light">
+            {description
+              .filter((desc) => desc.trim() !== '')
+              .map((desc, index) => (
+                <p key={index}>{desc}</p>
+              ))}
+          </div>
+        ) : null}
+      </div>
+    </section>
+  );
+}
