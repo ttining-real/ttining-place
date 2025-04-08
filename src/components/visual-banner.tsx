4b778eb4e013c00f9879fee1e96a3a1d3633a149
ttining <ttining.real@gmail.com>
2025-04-08 17:49:05 +0900

    [Modify] 비주얼 배너 컴포넌트 수정
    - 다크모드 시, 텍스트 색상 유지(white)

--- src/components/visual-banner.tsx
+++ src/components/visual-banner.tsx
@@ -31,18 +31,18 @@ export default function VisualBanner() {
       className="flex h-[360px] flex-col items-center justify-center gap-6 bg-[url('/images/banner.png')] bg-cover bg-center bg-no-repeat px-6 bg-blend-multiply"
     >
       <h2
-        className={`${gmarket.className} text-center text-2xl font-bold text-white`}
+        className={`${gmarket.className} text-center text-2xl font-bold text-white dark:text-black`}
       >
         Welcome! An Jiin Portfolio
       </h2>
-      <p className="font-base mt-[-8px] text-center font-normal text-white">
+      <p className="font-base mt-[-8px] text-center font-normal text-white dark:text-black">
         안녕하세요. UX/UI 디자인과 웹 퍼블리싱 경험을 바탕으로 <br /> 프론트엔드
         개발자로 도약 중인 안지인입니다.
       </p>
       <Link
         href="/about"
         aria-label="About 페이지로 이동"
-        className="bg-primary rounded-md px-4 py-1.5 text-base text-white"
+        className="bg-primary rounded-md px-4 py-1.5 text-base text-white dark:text-black"
       >
         Go About
       </Link>
