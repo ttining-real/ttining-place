b9441e17ee5ea2cd8b2434474cbc325e4db1a780
ttining <ttining.real@gmail.com>
2025-04-08 10:22:09 +0900

    [Feature] SubTitle 컴포넌트화

---
+++ src/components/sub-title.tsx
@@ -0,0 +1,41 @@
+import localFont from 'next/font/local';
+
+const gmarket = localFont({
+  src: [
+    {
+      path: '../fonts/GmarketSansTTFBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/GmarketSansTTFMedium.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/GmarketSansTTFLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--gmarketFont',
+});
+
+type SubTitleProps = {
+  title: string;
+  description: string;
+};
+
+export default function SubTitle({ title, description }: SubTitleProps) {
+  return (
+    <>
+      <header className="flex flex-col items-center gap-1">
+        <h3 className={`${gmarket.className} gmarket text-2xl font-bold`}>
+          {title}
+        </h3>
+        <p>{description}</p>
+      </header>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,16 @@
+import SubTitle from '@/components/sub-title';
 import VisualBanner from '@/components/visual-banner';
-import localFont from 'next/font/local';
-
-const gmarket = localFont({
-  src: [
-    {
-      path: '../fonts/GmarketSansTTFBold.ttf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/GmarketSansTTFMedium.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/GmarketSansTTFLight.ttf',
-      weight: '200',
-      style: 'normal',
-    },
-  ],
-  display: 'swap',
-  variable: '--gmarketFont',
-});
 
 export default function Home() {
   return (
     <>
       <VisualBanner />
       <section className="m-auto flex max-w-5xl flex-col gap-6 py-12">
-        <header className="flex flex-col items-center gap-1">
-          <h3 className={`${gmarket.className} gmarket text-2xl font-bold`}>
-            Projects
-          </h3>
-          <p>Front-end Development Project</p>
-        </header>
+        <SubTitle
+          title="Projects"
+          description="Front-end Development Project"
+        />
+
         <div className="grid grid-cols-2 gap-6">
           <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
           <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
@@ -42,12 +19,7 @@ export default function Home() {
         </div>
       </section>
       <section className="m-auto flex max-w-5xl flex-col gap-6 py-12">
-        <header className="flex flex-col items-center gap-1">
-          <h3 className={`${gmarket.className} gmarket text-2xl font-bold`}>
-            Experience
-          </h3>
-          <p>Companies and Freelance</p>
-        </header>
+        <SubTitle title="Experience" description="Companies and Freelance" />
         <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
       </section>
     </>
