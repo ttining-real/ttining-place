1a68b42f35372c322cac97feb316a523cc1dc066
ttining <ttining.real@gmail.com>
2025-04-09 11:16:58 +0900

    [Modify] 페이지 타이틀 컴포넌트 수정
    - `description` 매개변수 선택 항목으로 변경
    - 스타일 추가

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -2,7 +2,7 @@ import localFont from 'next/font/local';
 
 type pageTitleProps = {
   title: string;
-  description: string[];
+  description?: string[];
 };
 
 const gmarket = localFont({
@@ -31,16 +31,16 @@ export default function PageTitle({ title, description }: pageTitleProps) {
   return (
     <section className="flex h-[360px] items-center justify-center bg-white bg-[url('/images/sample_about.png')] bg-cover bg-center bg-no-repeat">
       <div className="m-auto flex max-w-5xl flex-1 flex-col items-center justify-between gap-4 px-6 md:flex-row">
-        <div>
+        <div className="flex flex-col items-center gap-4">
           <h2
-            className={`${gmarket.className} flex flex-col items-center gap-2 text-3xl font-bold`}
+            className={`${gmarket.className} flex flex-col items-center gap-2 text-3xl font-bold text-white`}
           >
             {title}
           </h2>
           <hr className="w-[80px] border-2 border-white/50 dark:border-black/50" />
         </div>
         {description ? (
-          <div className="text-center font-light">
+          <div className="text-center font-light text-white">
             {description
               .filter((desc) => desc.trim() !== '')
               .map((desc, index) => (
