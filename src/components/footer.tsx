6c0530db0097045e8a5ee6e2f0027b4d7c16537b
ttining <ttining.real@gmail.com>
2025-04-11 12:05:41 +0900

    [Modify] 푸터 컴포넌트 수정
    - 반응형 스타일링 추가

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="text-gray-10 flex items-center justify-center gap-4 border-t-1 border-gray-50 py-8 text-sm">
+    <footer
+      role="contentinfo"
+      className="text-gray-10 flex flex-col border-t border-gray-50 p-4 text-center text-sm font-normal md:flex-row md:items-center md:justify-center md:gap-2"
+    >
       <span>&copy; 2025 ttinint place</span>
-      <span className="bg-gray-10 h-2.5 w-[1px]" aria-hidden={true}></span>
+      <span
+        className="md:bg-gray-30 hidden md:block md:h-2.5 md:w-[1px]"
+        aria-hidden={true}
+      ></span>
       <span>Designed & Developed by 안지인</span>
     </footer>
   );
