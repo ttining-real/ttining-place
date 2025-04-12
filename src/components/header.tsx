e84743f52b7182cee2a0ad6302fa28727ebd404c
ttining <ttining.real@gmail.com>
2025-04-12 10:29:12 +0900

    [Modify] header 컴포넌트 분리
    - 해상도에 따라 Nav 컴포넌트 조건부 렌더링

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,58 @@
-import localFont from 'next/font/local';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import StackIcon from './stack-icon';
 import LogoIcon from './logo-icon';
-
-const gmarket = localFont({
-  src: [
-    {
-      path: '../fonts/GmarketSansTTFBold.ttf',
-      weight: '700',
-    },
-    {
-      path: '../fonts/GmarketSansTTFMedium.ttf',
-      weight: '400',
-    },
-    {
-      path: '../fonts/GmarketSansTTFLight.ttf',
-      weight: '100',
-    },
-  ],
-  display: 'swap',
-  variable: '--gmarketFont',
-});
+import NavDesktop from './nav-desktop';
+import NavMobile from './nav-mobile';
+import Link from 'next/link';
 
 export default function Header() {
-  const pathname = usePathname();
+  const [isMobile, setIsMobile] = useState(false);
+
+  // 화면 크기 변경 감지
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 768px)'); // 모바일 기준
+    setIsMobile(mediaQuery.matches);
+
+    const handleResize = (e: MediaQueryListEvent) => {
+      setIsMobile(e.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleResize);
+    return () => mediaQuery.removeEventListener('change', handleResize);
+  }, []);
+
+  const navList = [
+    {
+      type: 'link',
+      href: '/about',
+      name: 'About',
+    },
+    {
+      type: 'link',
+      href: '/experience',
+      name: 'Experience',
+    },
+    {
+      type: 'link',
+      href: '/projects',
+      name: 'Projects',
+    },
+    {
+      type: 'button',
+      href: '/contact',
+      name: 'Contact',
+    },
+    {
+      type: 'external link',
+      href: 'https://github.com/ttining-real',
+      name: 'GitHub',
+    },
+    {
+      type: 'external link',
+      href: 'https://velog.io/@ttining/posts',
+      name: 'Velog',
+    },
+  ];
+
   const [theme, setTheme] = useState<'light' | 'dark'>('light');
 
   useEffect(() => {
@@ -41,74 +67,19 @@ export default function Header() {
     return () => mediaQuery.removeEventListener('change', handleChange);
   }, []);
 
-  const nav = [
-    {
-      href: '/about',
-      name: 'About',
-    },
-    {
-      href: '/experience',
-      name: 'Experience',
-    },
-    {
-      href: '/projects',
-      name: 'Projects',
-    },
-    {
-      href: '/contact',
-      name: 'Contact',
-    },
-    {
-      href: 'https://github.com/ttining-real',
-      name: 'GitHub',
-    },
-    {
-      href: 'https://velog.io/@ttining/posts',
-      name: 'Velog',
-    },
-  ];
+  // const onClickContact = () => {
+  //   console.log('Contact Modal 출력');
+  // };
 
   return (
-    <header className="flex h-14 items-center justify-between gap-6 bg-white px-6 shadow-sm">
+    <header className="fixed top-0 right-0 left-0 flex h-14 items-center justify-between gap-6 bg-white px-6 shadow-sm">
       <h1>
         <Link href="/" aria-label="홈으로 이동">
           <LogoIcon id="light" />
         </Link>
       </h1>
-      <nav
-        className={`${gmarket.className} font-gmarket`}
-        aria-label="주요 메뉴"
-      >
-        <ul className="flex items-center gap-1">
-          {nav.map((menu, index) => {
-            const isExternal = menu.href.startsWith('http');
-
-            return (
-              <li key={index}>
-                {isExternal ? (
-                  <a
-                    href={menu.href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex h-14 items-center px-4 text-base font-normal text-black"
-                    aria-label={`안지인의 ${menu.name}으로 이동`}
-                  >
-                    <StackIcon id={menu.name} />
-                  </a>
-                ) : (
-                  <Link
-                    href={menu.href}
-                    className="flex h-14 items-center px-4 text-base font-normal text-black"
-                    aria-current={pathname === menu.href ? 'page' : undefined}
-                  >
-                    {menu.name}
-                  </Link>
-                )}
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      {/* 화면 크기 변화에 따라 Nav 컴포넌트 변경 */}
+      {isMobile ? <NavMobile nav={navList} /> : <NavDesktop nav={navList} />}
     </header>
   );
 }
