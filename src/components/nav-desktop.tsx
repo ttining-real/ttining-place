9b03dd44063b8c0300af043c08905fd93b1d2bc2
ttining <ttining.real@gmail.com>
2025-04-12 10:28:03 +0900

    [Component] NavDesktop 컴포넌트 생성
    - 기존 header 컴포넌트 내 nav 분리

---
+++ src/components/nav-desktop.tsx
@@ -0,0 +1,72 @@
+import localFont from 'next/font/local';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import StackIcon from './stack-icon';
+
+const gmarket = localFont({
+  src: [
+    {
+      path: '../fonts/GmarketSansTTFBold.ttf',
+      weight: '700',
+    },
+    {
+      path: '../fonts/GmarketSansTTFMedium.ttf',
+      weight: '400',
+    },
+    {
+      path: '../fonts/GmarketSansTTFLight.ttf',
+      weight: '100',
+    },
+  ],
+  display: 'swap',
+  variable: '--gmarketFont',
+});
+
+export default function NavDesktop({ nav }) {
+  const pathname = usePathname();
+
+  return (
+    <nav className={`${gmarket.className} font-gmarket`} aria-label="주요 메뉴">
+      <ul className="flex items-center gap-1">
+        {nav.map((menu) => {
+          const isLink = menu.type === 'link';
+          const isButton = menu.type === 'button';
+          const isExternal = menu.type === 'external link';
+
+          return (
+            <li key={menu.name}>
+              {isLink ? (
+                <Link
+                  href={menu.href}
+                  aria-current={pathname === menu.href ? 'page' : undefined}
+                  className="flex h-14 items-center px-4 text-base font-normal text-black"
+                >
+                  {menu.name}
+                </Link>
+              ) : null}
+              {isButton ? (
+                <button
+                  // onClick={onClickContact}
+                  className="flex h-14 items-center px-4 text-base font-normal text-black"
+                >
+                  {menu.name}
+                </button>
+              ) : null}
+              {isExternal ? (
+                <a
+                  href={menu.href}
+                  target="_black"
+                  rel="noopener noreferrer"
+                  className="flex h-14 items-center px-4 text-base font-normal text-black"
+                  aria-label={`안지인의 ${menu.name}으로 이동`}
+                >
+                  <StackIcon id={menu.name} />
+                </a>
+              ) : null}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
