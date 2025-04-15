23d2d0a3ca17b54c9b963a29c7c3bebc4ba8802f
ttining <ttining.real@gmail.com>
2025-04-15 11:48:38 +0900

    [Chore] 이미지 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [process.env.NEXT_PUBLIC_IMAGE_DOMAIN!],
+  },
 };
 
 export default nextConfig;
