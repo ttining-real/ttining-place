81bb9a1396da302d74a8660f5a4fc5152c2bee8b
ttining <ttining.real@gmail.com>
2025-04-15 09:19:56 +0900

    [Page] Not Found 페이지 생성

---
+++ src/pages/404.tsx
@@ -0,0 +1,38 @@
+import Button from '@/components/button';
+import localFont from 'next/font/local';
+
+const gmarket = localFont({
+  src: [
+    {
+      path: '../fonts/GmarketSansTTFBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/GmarketSansTTFMedium.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/GmarketSansTTFLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--gmarketFont',
+});
+
+export default function Page() {
+  return (
+    <div className="flex h-full min-h-[calc(100vh-130px)] flex-col items-center justify-center gap-4 md:min-h-[calc(100vh-110px)]">
+      <h2 className={`${gmarket.className} text-primary text-xl font-bold`}>
+        Page Not Found 😶‍🌫️
+      </h2>
+      <p>존재하지 않는 페이지입니다.</p>
+      <Button type="link" path="/" className="mt-2">
+        홈 화면으로 돌아가기
+      </Button>
+    </div>
+  );
+}
