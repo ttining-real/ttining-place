8a87bb6b8293b0223c7fbe7a708aee6bdc79ac68
ttining <ttining.real@gmail.com>
2025-04-18 15:14:52 +0900

    [Style] 인덱스 페이지 정리
    - 불필요한 코드 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,9 @@
-import Card from '@/components/card';
-import SectionTitle from '@/components/section-title';
-import VisualBanner from '@/components/visual-banner';
-
-const cardData = [
-  {
-    title: '폴짝',
-    description: '여행지 추천 서비스',
-    imgUrl: '/images/example.gif',
-    size: 500,
-  },
-  {
-    title: 'Fitculator',
-    description: 'B2C 운동 서비스',
-    imgUrl: '/images/example.gif',
-    size: 500,
-  },
-  {
-    title: '파티구함',
-    description: '스터디 커뮤니케이션 서비스',
-    imgUrl: '/images/example.gif',
-    size: 500,
-  },
-  {
-    title: '마켓칼리',
-    description: "'마켓컬리' 클론 코딩",
-    imgUrl: '/images/example.gif',
-    size: 500,
-  },
-];
+import Introduce from '@/components/introduce';
 
 export default function Home() {
   return (
     <>
-      <VisualBanner />
-      <section className="m-auto flex max-w-5xl flex-col gap-6 px-6 py-12">
-        <SectionTitle
-          title="Projects"
-          description="Front-end Development Project"
-        />
-
-        <div className="grid-cols- grid gap-6 md:grid-cols-2">
-          {cardData.map((card, index) => (
-            <Card
-              key={index}
-              title={card.title}
-              description={card.description}
-              imgUrl={card.imgUrl}
-              size={card.size}
-            />
-          ))}
-        </div>
-      </section>
-      <section className="m-auto flex max-w-5xl flex-col gap-6 px-6 py-12">
-        <SectionTitle
-          title="Experience"
-          description="Companies and Freelance"
-        />
-        <div className="bg-gray-10 h-[380px] rounded-2xl"></div>
-      </section>
+      <Introduce />
     </>
   );
 }
