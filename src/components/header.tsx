305d690753e1a3c224f027af17627026529c337e
ttining <ttining.real@gmail.com>
2025-04-21 19:34:57 +0900

    [Modify] 헤더 컴포넌트 수정
    - 내비게이션 제거
    - 테마 전환 기능 추가

---
+++ src/components/header.tsx
@@ -0,0 +1,58 @@
+import Icon from './icon';
+import LogoIcon from './logo-icon';
+
+export default function Header({
+  isDark,
+  setIsDark,
+}: {
+  isDark: boolean;
+  setIsDark: (val: boolean) => void;
+}) {
+  const toggleTheme = () => {
+    const newTheme = isDark ? 'light' : 'dark';
+
+    document.documentElement.classList.toggle('dark', !isDark);
+    localStorage.setItem('theme', newTheme);
+    setIsDark(!isDark);
+  };
+
+  return (
+    <header className="bg-gray-60/30 border-gray-40 fixed right-0 left-0 border-b-[1px] px-6 py-2 backdrop-blur-md">
+      <div className="m-auto flex max-w-7xl items-center justify-between">
+        <h1 aria-label="안지인의 포트폴리오">
+          <LogoIcon id={`${isDark ? 'dark' : 'light'}`} />
+        </h1>
+        <div className="flex items-center gap-6">
+          {/* 다크모드 토글 스위치 */}
+          <div className="flex items-center gap-2">
+            <Icon
+              id={`mode-${isDark ? 'light' : 'dark'}`}
+              className="text-gray-20"
+            />
+            <button
+              onClick={toggleTheme}
+              aria-label="다크 모드 전환"
+              className={`relative h-6 w-12 rounded-full transition-colors duration-300 ${
+                isDark ? 'bg-gray-30' : 'bg-gray-30'
+              }`}
+            >
+              <span
+                className={`absolute top-[4px] left-[4px] h-4 w-4 rounded-full bg-white transition-transform duration-300 ${
+                  isDark ? 'translate-x-6' : ''
+                }`}
+              />
+            </button>
+          </div>
+          <a
+            href="#"
+            download
+            className="bg-primary flex h-[40px] items-center gap-1 rounded-lg px-3 text-sm font-normal text-white"
+          >
+            <Icon id="download" className="text-white" />
+            이력서 다운로드
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/layouts/global-layout.tsx
+++ src/layouts/global-layout.tsx
@@ -1,7 +1,8 @@
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import ScrollProgressBar from '@/components/progress-bar';
 import localFont from 'next/font/local';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 const pretendard = localFont({
   src: '../fonts/PretendardVariable.woff2',
@@ -10,15 +11,32 @@ const pretendard = localFont({
 });
 
 export default function GlobalLayout({ children }: { children: ReactNode }) {
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+
+    if (storedTheme === 'dark') {
+      document.documentElement.classList.add('dark');
+      setIsDark(true);
+    } else if (storedTheme === 'light') {
+      document.documentElement.classList.remove('dark');
+      setIsDark(false);
+    } else {
+      const prefersDark = window.matchMedia(
+        '(prefers-color-scheme: dark)',
+      ).matches;
+      document.documentElement.classList.toggle('dark', prefersDark);
+      setIsDark(prefersDark);
+    }
+  }, []);
+
   return (
     <>
-      <Header />
-      <div
-        className={`${pretendard.className} mt-14 flex h-[calc(100%-56px)] flex-col overflow-auto`}
-      >
-        <main>{children}</main>
-        <Footer />
-      </div>
+      <Header isDark={isDark} setIsDark={setIsDark} />
+      <ScrollProgressBar />
+      <main className={`${pretendard.className} pt-[56px]`}>{children}</main>
+      <Footer />
     </>
   );
 }
