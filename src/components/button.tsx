8f47d30539ea6a480bee7cc00c26d70c7243d311
ttining <ttining.real@gmail.com>
2025-04-23 17:11:29 +0900

    [Modify] 버튼 컴포넌트 수정
    - 타입 정의 추가
    - 타입별 분기 처리

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,57 +1,89 @@
 import Link from 'next/link';
 import React, { ReactNode } from 'react';
 
-type BaseProps = {
+const baseStyle =
+  'flex items-center justify-center gap-1 rounded-4xl text-base font-normal border-[1px] whitespace-nowrap hover:font-medium';
+
+const buttonSize: Record<string, string> = {
+  sm: 'w-8 h-8',
+  md: 'h-12 px-4',
+};
+
+const variantStyle: Record<string, string> = {
+  primary: 'bg-primary text-white border-white/10 hover:brightness-130',
+  secondary: 'bg-transparent text-primary border-primary hover:bg-primary/20',
+  tertiary: 'bg-gray-50 text-black border-white/10 hover:bg-gray-40',
+};
+
+type CommonButtonTypes = {
   children: ReactNode;
+  size?: keyof typeof buttonSize;
+  variant?: keyof typeof variantStyle;
   className?: string;
 };
 
-type ButtonProps =
-  | (BaseProps & { type: 'button' })
-  | (BaseProps & { type: 'link' | 'external'; path: string });
+type LinkButtonTypes = CommonButtonTypes & {
+  type: 'link';
+  href: string;
+};
 
-// children에서 텍스트 추출
-function extractText(children: ReactNode): string {
-  if (typeof children === 'string') {
-    return children;
+type DownloadButtonTypes = CommonButtonTypes & {
+  type: 'download';
+  href: string;
+};
+
+type ExternalButtonTypes = CommonButtonTypes & {
+  type: 'external';
+  href: string;
+};
+
+type NormalButtonTypes = CommonButtonTypes & {
+  type: 'button';
+  onClick?: () => void;
+};
+
+type ButtonTypes =
+  | LinkButtonTypes
+  | DownloadButtonTypes
+  | ExternalButtonTypes
+  | NormalButtonTypes;
+
+export default function Button(props: ButtonTypes) {
+  const { type, size = 'md', variant = 'primary', className, children } = props;
+  const style = `${baseStyle} ${buttonSize[size]} ${variantStyle[variant]} ${className ?? ''}`;
+
+  switch (type) {
+    case 'link':
+      return (
+        <Link href={props.href} className={style}>
+          {children}
+        </Link>
+      );
+    case 'download':
+      return (
+        <a href={props.href} download className={style}>
+          {children}
+        </a>
+      );
+    case 'external':
+      return (
+        <a
+          href={props.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={style}
+        >
+          {children}
+        </a>
+      );
+    case 'button':
+      return (
+        <button className={style} onClick={props.onClick}>
+          {children}
+        </button>
+      );
+
+    default:
+      return null;
   }
-  return '';
-}
-
-export default function Button(props: ButtonProps) {
-  const { className = '', children } = props;
-  const buttonStyle = `bg-primary text-white px-4 py-2 rounded-lg text-base ${className}`;
-  const ariaLabel = extractText(children);
-
-  if (props.type === 'button') {
-    return (
-      <button type="button" className={buttonStyle}>
-        {children}
-      </button>
-    );
-  }
-
-  if (props.type === 'link') {
-    return (
-      <Link href={props.path} className={buttonStyle} aria-label={ariaLabel}>
-        {children}
-      </Link>
-    );
-  }
-
-  if (props.type === 'external') {
-    return (
-      <a
-        href={props.path}
-        className={buttonStyle}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label={ariaLabel}
-      >
-        {children}
-      </a>
-    );
-  }
-
-  return null;
 }
