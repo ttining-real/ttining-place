08c30ff6cf1b98be985a5efe25479bfe67e8d464
ttining <ttining.real@gmail.com>
2025-04-25 20:04:16 +0900

    [Modify] footer 컴포넌트 수정

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -1,15 +1,32 @@
+import Link from 'next/link';
+import LogoIcon from '../icon/logo-icon';
+
 export default function Footer() {
   return (
     <footer
       role="contentinfo"
-      className="text-gray-10 flex flex-col border-t border-gray-50 p-4 text-center text-sm font-normal md:flex-row md:items-center md:justify-center md:gap-2"
+      className="text-gray-10 flex flex-col items-center justify-center gap-1 border-t border-gray-50 px-8 py-4 text-center text-[13px] sm:gap-2 sm:text-sm"
     >
-      <span>&copy; 2025 ttinint place</span>
-      <span
-        className="md:bg-gray-30 hidden md:block md:h-2.5 md:w-[1px]"
-        aria-hidden={true}
-      ></span>
-      <span>Designed & Developed by 안지인</span>
+      <div className="flex flex-col items-center gap-1 sm:flex-row sm:gap-2">
+        <p className="flex items-center gap-1">
+          <span>&copy; 2025</span>
+          <Link href="/">
+            <LogoIcon
+              id="light"
+              width={36}
+              height={18}
+              className="mt-1 opacity-30"
+            />
+          </Link>
+          <span>All rights reserved.</span>
+        </p>
+        <span
+          className="sm:bg-gray-30 hidden sm:block sm:h-2.5 sm:w-[1px]"
+          aria-hidden={true}
+        ></span>
+        <p>Designed & Developed by 안지인</p>
+      </div>
+      <p>본 웹사이트는 개인 포트폴리오 목적으로 제작되었습니다.</p>
     </footer>
   );
 }
