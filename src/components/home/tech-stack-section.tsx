6e73072d045fec686be6c27a7049bba8ba5e8a11
ttining <ttining.real@gmail.com>
2025-04-26 12:10:53 +0900

    [Modify] tech-stack 컴포넌트 수정

--- src/components/home/tech-stack-section.tsx
+++ src/components/home/tech-stack-section.tsx
@@ -1,50 +1,65 @@
-import { gmarket } from '@/fonts/font';
-import Button from '../button';
-import Icon from '../icon/icon';
 import StackIcon from '../icon/stack-icon';
+import Link from 'next/link';
+import { gmarket } from '@/fonts/font';
 import { StackDataTypes } from '@/types/tech-stack-types';
 
 export default function TechStackSection({ data }: { data: StackDataTypes[] }) {
+  const formatNumber = (i: number) => {
+    if (i < 10) {
+      return `0${i}`;
+    }
+  };
+
   return (
     <section
       className={`bg-[url(/images/tech-stack.png)] bg-cover bg-fixed bg-bottom bg-no-repeat px-6 py-16`}
     >
       <div className="m-auto flex max-w-5xl flex-col gap-6">
-        <h2 className={`${gmarket.className} text-2xl font-bold text-white`}>
+        <h2
+          className={`${gmarket.className} text-xl font-bold text-white sm:text-2xl`}
+        >
           Tech Stack
         </h2>
         <div className="flex flex-wrap gap-6">
           {data.map((data, index) => (
             <article
               key={data.id}
-              className="flex flex-1 flex-col gap-2 rounded-3xl bg-white/25 p-6 backdrop-blur-md"
+              aria-labelledby={`tech-stack-title-${index}`}
+              className="flex flex-1 flex-col gap-2 rounded-3xl bg-white/15 backdrop-blur-md transition-all duration-300 hover:bg-white/25"
             >
-              <div className="order-2 flex flex-col gap-2">
-                <h3 className="text-xl font-bold text-white">{data.title}</h3>
-                <ul className="flex flex-wrap gap-2">
-                  {data.stack_items.map((item) =>
-                    item.stack_icons.map((icon) => (
-                      <li key={icon.id}>
-                        <StackIcon id={icon.name} size={16} />
-                      </li>
-                    )),
-                  )}
-                </ul>
-              </div>
-              <div className="order-1 flex items-center justify-between">
-                <span className="text-2xl font-bold text-white/50">
-                  {`0${index + 1}`}
-                </span>
-                <Button
-                  type="link"
-                  size="sm"
-                  href={`/tech-stack#${data.title}`}
-                  variant="tertiary"
-                  className="hover:text-primary bg-white/20 text-white/70 hover:bg-white/40"
+              <Link
+                href={`/tech-stack/#${String(data.title).toLowerCase()}`}
+                className="h-full p-6"
+              >
+                <p className="text-base font-semibold text-white/50 sm:text-lg">
+                  {formatNumber(index + 1)}
+                </p>
+                <h3
+                  id={`tech-stack-title-${index}`}
+                  className="text-xl font-bold text-white sm:text-xl"
                 >
-                  <Icon id="direction-right" />
-                </Button>
-              </div>
+                  {data.title}
+                </h3>
+                {Array.isArray(data.stack_items) &&
+                  data.stack_items.some(
+                    (item) =>
+                      Array.isArray(item.stack_icons) &&
+                      item.stack_icons.length > 0,
+                  ) && (
+                    <ul
+                      aria-label={`${data.title} 관련 기술 스택 아이콘`}
+                      className="mt-1 flex flex-wrap gap-2"
+                    >
+                      {data.stack_items.map((item) =>
+                        item.stack_icons.map((icon) => (
+                          <li key={icon.id} aria-label={`${icon.name} 아이콘`}>
+                            <StackIcon id={icon.name} size={16} />
+                          </li>
+                        )),
+                      )}
+                    </ul>
+                  )}
+              </Link>
             </article>
           ))}
         </div>
