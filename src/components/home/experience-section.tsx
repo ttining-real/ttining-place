5db12de6693d6648b26b7c625e534c4dfc442fb9
ttining <ttining.real@gmail.com>
2025-04-27 21:27:30 +0900

    [Feature] experience 섹션 생성

---
+++ src/components/home/experience-section.tsx
@@ -0,0 +1,24 @@
+import { gmarket } from '@/fonts/font';
+import { CareersDataTypes } from '@/types/career-types';
+import Card from '../card';
+
+export default function ExperienceSection({
+  data,
+}: {
+  data: CareersDataTypes[];
+}) {
+  return (
+    <section className="bg-white py-8">
+      <div className="m-auto flex max-w-5xl flex-col gap-6 p-6 sm:gap-8">
+        <h2
+          className={`${gmarket.className} text-xl font-bold text-black sm:text-2xl`}
+        >
+          Experience
+        </h2>
+        <div className="flex gap-2 sm:gap-4">
+          <Card data={data} />
+        </div>
+      </div>
+    </section>
+  );
+}
