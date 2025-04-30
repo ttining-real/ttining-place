341d13829b7ef33a0e327867770bf225d18c9448
ttining <ttining.real@gmail.com>
2025-04-30 20:55:02 +0900

    [Modify] LogoIcon 컴포넌트 외 수정
    - 사이즈 props 추가

--- src/components/icon/logo-icon.tsx
+++ src/components/icon/logo-icon.tsx
@@ -1,13 +1,20 @@
 type LogoIconProps = {
   id: 'light' | 'dark';
+  width?: number;
+  height?: number;
   className?: string;
 };
 
-export default function LogoIcon({ id, className }: LogoIconProps) {
+export default function LogoIcon({
+  id,
+  width = 80,
+  height = 40,
+  className,
+}: LogoIconProps) {
   return (
     <svg
-      width={80}
-      height={40}
+      width={width}
+      height={height}
       viewBox="0 0 80 40"
       className={className}
       aria-hidden={true}
