5234bc5ff5f8e1ff88dc5317da70fbc50a0f40f6
ttining <ttining.real@gmail.com>
2025-05-15 19:01:17 +0900

    [Modify] Chip 컴포넌트 수정
    - Chip export 키워드 추가

--- src/components/chips.tsx
+++ src/components/chips.tsx
@@ -9,7 +9,7 @@ type ChipsProps = {
   chipsClassName?: string;
 };
 
-function Chip({ text, className = '' }: ChipProps) {
+export function Chip({ text, className = '' }: ChipProps) {
   return (
     <li
       className={`dark:border-gray-20 rounded-4xl border border-gray-50 px-3 py-1 text-[13px] whitespace-nowrap text-black md:text-sm dark:text-white ${className}`}
