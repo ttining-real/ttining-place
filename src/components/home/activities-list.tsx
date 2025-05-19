1d110f2353aa156f3aebf732071852696d81d183
ttining <ttining.real@gmail.com>
2025-05-19 13:31:23 +0900

    [Modify] ActivitiesList 스타일 수정

--- src/components/home/activities-list.tsx
+++ src/components/home/activities-list.tsx
@@ -10,17 +10,17 @@ export default function ActivitiesList({
   data: ActivitiesDataTypes[];
 }) {
   return (
-    <ul className="flex w-full flex-col gap-2">
+    <ul className="flex w-full flex-col gap-4 sm:gap-2">
       {data.map((data, index) => (
         <li
           key={index}
-          className="hover:border-primary/60 w-full rounded-lg border-2 border-white bg-white transition-all duration-300"
+          className="hover:border-primary/60 w-full rounded-lg border-2 border-white bg-white shadow-lg transition-all duration-300"
         >
           <Link
             href={`/activities/#${data.company_name}`}
-            className="flex flex-col justify-between p-6 sm:flex-row"
+            className="flex flex-col justify-between p-6 sm:flex-row sm:items-center"
           >
-            <div className="flex flex-col gap-1 sm:flex-row sm:gap-3">
+            <div className="flex flex-col gap-1 sm:flex-row sm:items-center sm:gap-3">
               <span className="text-primary/60 font-bold">
                 {formatIndex(index)}
               </span>
