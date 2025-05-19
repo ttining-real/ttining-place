ec18db658a47d4010bf706d8686136d8b0aee048
ttining <ttining.real@gmail.com>
2025-05-19 13:32:06 +0900

    [Modify] TagListSection - Stack 관련 수정
    - Chip, StackIcon 컴포넌트 사용

--- src/components/chips.tsx
+++ src/components/chips.tsx
@@ -1,19 +1,24 @@
+import StackIcon from './icon/stack-icon';
+
 type ChipProps = {
+  icon: boolean;
   text: string;
   className?: string;
 };
 
 type ChipsProps = {
   data: string[];
+  icon: boolean;
   chipClassName?: string;
   chipsClassName?: string;
 };
 
-export function Chip({ text, className = '' }: ChipProps) {
+export function Chip({ icon = false, text, className = '' }: ChipProps) {
   return (
     <li
-      className={`dark:border-gray-20 rounded-4xl border border-gray-50 px-3 py-1 text-[13px] whitespace-nowrap text-black md:text-sm dark:text-white ${className}`}
+      className={`dark:border-gray-20 flex items-center gap-1.5 rounded-4xl border border-gray-50 px-3 py-1 text-[13px] whitespace-nowrap text-black md:text-sm dark:text-white ${className}`}
     >
+      {icon ? <StackIcon id={text} size={16} /> : null}
       {text}
     </li>
   );
@@ -21,13 +26,14 @@ export function Chip({ text, className = '' }: ChipProps) {
 
 export default function Chips({
   data,
+  icon = false,
   chipClassName = '',
   chipsClassName = '',
 }: ChipsProps) {
   return (
     <ul className={`flex flex-wrap gap-x-1 gap-y-1.5 ${chipsClassName}`}>
       {data.map((text, index) => (
-        <Chip key={index} text={text} className={chipClassName} />
+        <Chip icon={icon} key={index} text={text} className={chipClassName} />
       ))}
     </ul>
   );
--- src/components/experience/tag-list-section.tsx
+++ src/components/experience/tag-list-section.tsx
@@ -1,3 +1,5 @@
+import Chips from '../chips';
+
 type TagListSection = {
   label: string;
   contents: string | string[];
@@ -11,23 +13,33 @@ export default function TagListSection({
   list = false,
   stack = false,
 }: TagListSection) {
+  console.log(contents);
+
   return (
     <div className="flex flex-col gap-1">
       <dt className="text-gray-10 font-medium">{label}</dt>
-      <dd className={``}>
+      <dd>
         {Array.isArray(contents) ? (
-          <ul
-            className={`${stack === true ? 'flex flex-wrap gap-x-1 gap-y-2 pt-1' : ''}`}
-          >
-            {contents.map((content, index) => (
-              <li
-                key={index}
-                className={`${list === true ? `before:bg-gray-10 relative pl-2 before:absolute before:top-1/2 before:left-[1px] before:h-[3px] before:w-[3px] before:-translate-y-1/2 before:transform before:rounded-sm before:content-['']` : ''} ${stack === true ? `border-gray-30 rounded-3xl border px-3 py-0.5` : ''}`}
-              >
-                {content}
-              </li>
-            ))}
-          </ul>
+          stack === true ? (
+            <Chips icon={true} data={contents} />
+          ) : list === true ? (
+            <ul className="pt-1">
+              {contents.map((content, index) => (
+                <li
+                  key={index}
+                  className="before:bg-gray-10 relative pl-2 before:absolute before:top-1/2 before:left-[1px] before:h-[3px] before:w-[3px] before:-translate-y-1/2 before:transform before:rounded-sm before:content-['']"
+                >
+                  {content}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <ul>
+              {contents.map((content, index) => (
+                <li key={index}>{content}</li>
+              ))}
+            </ul>
+          )
         ) : (
           <p>{contents}</p>
         )}
