34403c926404aa1fc8be72067b06ebce2582302b
ttining <ttining.real@gmail.com>
2025-06-19 18:17:54 +0900

    [Remove] 불필요한 파일 제거
    - 유틸 함수 파일 제거

--- src/lib/formatIndex.ts
+++
@@ -1,4 +0,0 @@
-export function formatIndex(i: number) {
-  const displayIndex = i + 1;
-  return displayIndex < 10 ? `0${displayIndex}` : `${displayIndex}`;
-}
--- src/lib/formetTitle.ts
+++
@@ -1,9 +0,0 @@
-export function titleFormatter(pathname: string): string | undefined {
-  if (pathname === '/' || pathname === '') return;
-
-  const segment = pathname.split('/')[1];
-
-  return segment
-    .replace(/-/g, ' ')
-    .replace(/\b\w/g, (char) => char.toUpperCase());
-}
