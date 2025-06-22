40ef14f4bd31e7905c88ecff1791791d5200a232
ttining <ttining.real@gmail.com>
2025-06-22 11:45:27 +0900

    [Modify] 폰트 파일 변경
    - Gmarket Sans 제거
    - Agbalumo 폰트 파일 추가

--- src/fonts/font.ts
+++ src/fonts/font.ts
@@ -6,24 +6,8 @@ export const pretendard = localFont({
   variable: '--pretendardFont',
 });
 
-export const gmarket = localFont({
-  src: [
-    {
-      path: './GmarketSansBold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: './GmarketSansMedium.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './GmarketSansLight.woff2',
-      weight: '200',
-      style: 'normal',
-    },
-  ],
+export const Agbalumo = localFont({
+  src: './Agbalumo-Regular.ttf',
   display: 'swap',
-  variable: '--gmarketFont',
+  variable: '--agbalumoFont',
 });
