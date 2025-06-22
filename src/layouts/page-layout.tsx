bf6b798c917286ab81fda0e80fb47559f8fad205
ttining <ttining.real@gmail.com>
2025-06-22 11:52:13 +0900

    [Modify] PageLayout 수정
    - 사용하지 않는 컴포넌트 제거
    - titleMap 추가
    - 현재 path에 따라 페이지 타이틀 결정하도록 수정

--- src/layouts/page-layout.tsx
+++ src/layouts/page-layout.tsx
@@ -1,31 +1,42 @@
-import Footer from '@/components/app/footer';
-import Header from '@/components/app/header';
-import ScrollProgressBar from '@/components/progress-bar';
-import TopButton from '@/components/top-button';
-import { gmarket } from '@/fonts/font';
-import { titleFormatter } from '@/lib/formetTitle';
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+import ToTheTop from '@/components/to-the-top';
+import { Agbalumo, pretendard } from '@/fonts/font';
+// import { usePathname } from 'next/navigation';
 import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 
 export default function PageLayout({ children }: { children: ReactNode }) {
   const router = useRouter();
-  const title = titleFormatter(router.pathname);
+  const pathname = router.asPath;
+
+  const titleMap: Record<string, string> = {
+    '/about': 'about',
+    '/careers': 'careers',
+    '/projects': 'projects',
+    '/example': 'example',
+  };
+
+  const matchedKey = Object.keys(titleMap).find((key) =>
+    pathname?.startsWith(key),
+  );
+  const pageTitle = matchedKey ? titleMap[matchedKey] : '';
+
   return (
     <>
       <Header />
-      <ScrollProgressBar />
-      <main className={`${gmarket.className}`}>
-        <div className="mt-[56px] flex h-24 items-end bg-[url(/images/page_title_background.png)] bg-cover sm:h-60">
-          <h2 className="mx-auto w-full max-w-5xl px-6 pb-6 text-lg font-bold text-white sm:pb-16 sm:text-3xl">
-            {title}
+      <main className={`${pretendard.className}`}>
+        <div
+          className={`${Agbalumo.className} h-full bg-[url(/images/bg_subpage.png)] bg-cover bg-center px-6 pt-14 pb-6`}
+        >
+          <h2 className="m-auto max-w-5xl px-6 pt-10 text-2xl text-white sm:pt-20 sm:text-4xl">
+            {pageTitle}
           </h2>
         </div>
-        <div className="m-auto flex max-w-5xl flex-col gap-12 p-6">
-          {children}
-        </div>
+        {children}
+        <ToTheTop />
       </main>
       <Footer />
-      <TopButton />
     </>
   );
 }
