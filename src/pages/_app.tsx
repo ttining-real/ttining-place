88bf424583d42c939a618cee4b1a7c855926f17d
ttining <ttining.real@gmail.com>
2025-06-23 14:50:46 +0900

    [Modify] App 컴포넌트 수정
    - 코드 정리
    - router.pathname 기반으로 레이아웃 선택

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
-'use client';
-
+import '@/styles/globals.css';
 import GlobalLayout from '@/layouts/global-layout';
 import PageLayout from '@/layouts/page-layout';
-import '@/styles/globals.css';
 import { NextPage } from 'next';
-
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactNode) => React.ReactNode;
@@ -19,15 +16,22 @@ export default function App({
 }: AppProps & {
   Component: NextPageWithLayout;
 }) {
+  const [isClient, setIsClient] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   // pathname 기반으로 레이아웃 선택
   const Layout = useMemo(() => {
+    if (!isClient) return GlobalLayout;
+
     if (router.pathname === '/') {
       return GlobalLayout;
     }
     return PageLayout;
-  }, [router.pathname]);
+  }, [isClient, router.pathname]);
 
   const getLayout = Component.getLayout ?? ((page) => page);
 
