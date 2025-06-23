43cff84e9b4d668f1b0680d887417ade0d4774a1
ttining <ttining.real@gmail.com>
2025-06-23 18:51:13 +0900

    [Feature] Button 컴포넌트 생성
    - Button 컴포넌트

---
+++ src/components/button.tsx
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+import { ReactNode } from 'react';
+
+type ButtonProps = {
+  variant?: 'primary' | 'secondary';
+  href?: string;
+  external?: boolean;
+  onClick?: () => void;
+  children: ReactNode;
+  className?: string;
+};
+
+export default function Button({
+  variant = 'primary',
+  href,
+  external,
+  onClick,
+  children,
+  className,
+}: ButtonProps) {
+  // 내부 링크 여부 판단 (href가 있고, '/'로 시작하면 내부)
+  const isInternalLink = href && !external && href.startsWith('/');
+
+  // variant별 클래스 분리
+  const variantClassMap = {
+    primary: 'bg-primary text-white',
+    secondary: 'bg-primary-lighter text-primary-darkest',
+  };
+
+  const baseClassName =
+    'px-4 py-2 text-sm rounded-lg cursor-pointer ease-in-out transition-colors duration-200 hover:brightness-110';
+
+  const variantClassName = variantClassMap[variant];
+
+  const combinedClassName = `${baseClassName} ${variantClassName} ${className}`;
+
+  if (isInternalLink) {
+    return (
+      <Link href={href} className={`${combinedClassName} ${className}`}>
+        {children}
+      </Link>
+    );
+  }
+
+  if (href && !isInternalLink) {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`${combinedClassName} ${className} `}
+      >
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <button onClick={onClick} className={`${combinedClassName} ${className}`}>
+      {children}
+    </button>
+  );
+}
