9a201dafc2180828a081818bc33c7e032efb6965
ttining <ttining.real@gmail.com>
2025-06-24 11:23:51 +0900

    [Chore] next 설정 파일 변경
    - Deprecated 설정 제거
    - images/remotePatterns 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.NEXT_PUBLIC_IMAGE_DOMAIN!],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lvadoslmpjidqgrxvkve.supabase.co',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
   },
 };
 
