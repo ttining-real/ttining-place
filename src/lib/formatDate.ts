7740d6852e6edc12c06385ec09633d15d14b77f5
ttining <ttining.real@gmail.com>
2025-06-24 17:33:10 +0900

    [Modify] lib/formatDate 유틸 함수 수정
    - day 제거

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,4 +1,4 @@
-// 날짜를 "YYYY. MM. DD" 형식으로 변환하는 함수
+// 날짜를 "YYYY년 MM월" 형식으로 변환하는 함수
 export function formatDate(dateString: string | Date | undefined): string {
   if (!dateString) {
     return '날짜 없음';
@@ -6,9 +6,10 @@ export function formatDate(dateString: string | Date | undefined): string {
 
   const date = new Date(dateString);
 
-  // 날짜 형식을 "YYYY. MM. DD"로 수동으로 조정
+  // 날짜 형식을 "YYYY년 MM월"로 수동으로 조정
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}. ${month}. ${day}`;
+  // const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}년 ${month}월`;
 }
