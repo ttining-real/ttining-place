14c783d4660b45aa4a69d0713498201346073627
ttining <ttining.real@gmail.com>
2025-06-25 13:53:24 +0900

    [Modify] home/circle 컴포넌트 수정
    - Link 태그 사용
    - SectionTitle 컴포넌트 사용
    - 스타일 수정

--- src/components/home/circle.tsx
+++ src/components/home/circle.tsx
@@ -1,5 +1,8 @@
-import { Agbalumo } from '@/fonts/font';
+import Link from 'next/link';
 import { useRef } from 'react';
+
+import { Agbalumo } from '@/fonts/font';
+import SectionTitle from '@/components/section-title';
 import { useGsapFadeInOnScroll } from '@/hooks/useGsapFadeInOnScroll';
 
 export default function CircleSection({ keywords }: { keywords: string[] }) {
@@ -7,18 +10,28 @@ export default function CircleSection({ keywords }: { keywords: string[] }) {
 
   useGsapFadeInOnScroll(containerRef);
 
+  const circleLayout =
+    'flex items-center justify-center rounded-full aspect-square -my-4 p-8';
+  const circleAnimate =
+    'transition-all duration-300 ease-out hover:scale-105 hover:border-2 hover:border-white/60 hover:text-xl hover:shadow-lg';
+
   return (
     <section className="bg-[url(/images/home/bg_circle.png)] bg-cover px-6 py-12">
+      <header>
+        <SectionTitle title="tech stack" className="sr-only" />
+      </header>
       <div ref={containerRef} className="m-auto max-w-[160px] sm:max-w-4xl">
         <ul
           className={`${Agbalumo.className} flex flex-col text-lg text-white sm:ml-12 sm:flex-row`}
         >
           {keywords.map((text) => (
-            <li
-              key={text}
-              className="gsap-fade-in -my-4 flex aspect-square items-center justify-center rounded-full border border-white p-8 sm:-mx-2 sm:w-1/4"
-            >
-              {text}
+            <li key={text} className={`gsap-fade-in sm:-mx-2 sm:w-1/4`}>
+              <Link
+                href={`/about/#${text}`}
+                className={`border border-white/80 ${circleLayout} ${circleAnimate}`}
+              >
+                {text}
+              </Link>
             </li>
           ))}
         </ul>
