d19ed6448c7b05769a148c8e6e632e82a130f823
ttining <ttining.real@gmail.com>
2025-07-04 14:15:09 +0900

    [Refactor] SectionTitle 컴포넌트 리팩토링
    - level 프롭스 추가
    - level에 따른 heading 태그 렌더링

--- src/components/section-title.tsx
+++ src/components/section-title.tsx
@@ -1,16 +1,35 @@
 import { Agbalumo } from '@/fonts/font';
 
 type SectionTitleProps = {
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
   title: string;
   className?: string;
 };
 
-export default function SectionTitle({ title, className }: SectionTitleProps) {
+const headings: Record<
+  1 | 2 | 3 | 4 | 5 | 6,
+  React.FC<React.HTMLAttributes<HTMLHeadingElement>>
+> = {
+  1: (props) => <h1 {...props} />,
+  2: (props) => <h2 {...props} />,
+  3: (props) => <h3 {...props} />,
+  4: (props) => <h4 {...props} />,
+  5: (props) => <h5 {...props} />,
+  6: (props) => <h6 {...props} />,
+};
+
+export default function SectionTitle({
+  level = 3,
+  title,
+  className = '',
+}: SectionTitleProps) {
+  const HeadingTag = headings[level];
+
   return (
-    <h3
+    <HeadingTag
       className={`${Agbalumo.className} text-primary xs:text-[32px] text-2xl ${className}`}
     >
       {title.toLowerCase()}
-    </h3>
+    </HeadingTag>
   );
 }
