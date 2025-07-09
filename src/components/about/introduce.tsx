d8658f790a1a1c953e7de311e34bad2329e318ab
ttining <ttining.real@gmail.com>
2025-07-09 12:20:10 +0900

    [Modify] About- Introduce 섹션 수정
    - 디자인 수정
    - constants/personal 수정
    - 컴포넌트, gsap 훅 제거

--- src/components/about/introduce.tsx
+++ src/components/about/introduce.tsx
@@ -1,76 +1,49 @@
-import Image from 'next/image';
-import React, { useRef } from 'react';
+import React from 'react';
 
+import { montserrat } from '@/fonts/font';
 import { contact } from '@/constants/personal';
-import SectionTitle from '@/components/section-title';
-import IconImg from '@/components/icon-image';
 import StackIcon from '@/components/stack-icon';
-import { useGsapFadeInOnScroll } from '@/hooks/useGsapFadeInOnScroll';
+import SectionLayout from '@/components/section-layout';
+import IconImg from '@/components/icon-image';
 
 export default function Introduce() {
-  const containerRef = useRef<HTMLDivElement>(null!);
-
-  useGsapFadeInOnScroll(containerRef);
-
-  const strongClassName =
-    'bg-primary/20 rounded-sm px-1 py-0.5 font-normal text-primary-darker';
-
   return (
-    <section id="introduce" className="py-12">
-      <div
-        ref={containerRef}
-        className="m-auto flex max-w-5xl flex-col justify-between gap-8 px-6 sm:flex-row sm:items-center sm:gap-10 md:gap-20"
-      >
-        <div className="flex flex-col gap-8">
-          <header className="gsap-fade-in flex items-baseline gap-4">
-            <SectionTitle title="introduce" />
-          </header>
-          <div className="gsap-fade-in flex flex-col">
-            <p>
-              안녕하세요. 사용자 중심의 웹 환경을 만들기 위해
-              <br />
-              디자인을 넘어 개발까지 탐구하며 성장하고 있는{' '}
-              <strong className={strongClassName}>안지인</strong>
-              입니다.
-            </p>
-
-            <p>
-              <strong className={strongClassName}>
-                7년간 UX/UI 디자인과 퍼블리싱 경험
-              </strong>
-              을 쌓으며
-              <br />
-              사용자 친화적인 인터페이스 설계와 반응형 웹 개발에 집중해왔습니다.
-            </p>
-
-            <p>
-              최근에는 프론트엔드 개발 영역까지 경험을 확장하며 컴포넌트 기반 UI
-              개발,
-              <br />
-              CRUD 기능 구현, 성능 최적화 등 실질적인 구현 능력을 키워가고
-              있습니다.
-            </p>
-
-            <p>
-              특히,{' '}
-              <strong className={strongClassName}>아토믹 디자인 시스템</strong>
-              을 활용한 구조적 설계와 시맨틱 태그 및<br />
-              ARIA 속성을 적극적으로 적용한 웹 접근성 향상에 관심이 많습니다.
-            </p>
-
-            <p>
-              기획부터 디자인, 퍼블리싱, 개발까지 전 과정을 아우르며,
-              <br />
-              직관적이고 편리한 사용자 경험을 구현하는{' '}
-              <strong className={strongClassName}>UI 개발자</strong>로 나아가고
-              있습니다.
-            </p>
-          </div>
-          <dl className="gsap-fade-in flex flex-col gap-2">
+    <>
+      <SectionLayout>
+        <header className={`${montserrat.className} `}>
+          <h3 className="mb-8 uppercase">Introduce</h3>
+        </header>
+        <div className="flex flex-col gap-6">
+          <p>
+            안녕하세요.
+            <br />
+            사용자 중심의 웹 환경을 만들기 위해 디자인을 넘어 개발까지 탐구하며
+            성장하고 있는 안지인입니다.
+            <br />
+            7년간 UX/UI 디자인과 퍼블리싱 경험을 쌓으며, 사용자 친화적인
+            인터페이스 설계와 반응형 웹 개발에 집중해왔습니다.
+            <br />
+            최근에는 프론트엔드 개발 영역까지 경험을 확장하며 컴포넌트 기반 UI
+            개발, CRUD 기능 구현, 성능 최적화 등 실질적인 구현 능력을 키워가고
+            있습니다.
+          </p>
+          <p>
+            특히, 아토믹 디자인 시스템을 활용한 구조적 설계와 시맨틱 태그 및
+            ARIA 속성을 적극적으로 적용한 웹 접근성 향상에 관심이 많습니다.
+            <br />
+            기획부터 디자인, 퍼블리싱, 개발까지 전 과정을 아우르며, 직관적이고
+            편리한 사용자 경험을 구현하는 UI 개발자로 나아가고 있습니다.
+            <br />
+            기술과 디자인의 경계에서 더 나은 사용자 경험을 설계하고 구현하는
+            안지인입니다. <br />약 7년간의 UX/UI 디자인과 퍼블리싱 경험을
+            바탕으로 사용자 중심의 웹을 고민하며 프론트엔드 개발자로 한 걸음씩
+            성장하고 있습니다.
+          </p>
+          <dl className="flex flex-col gap-2">
             {contact.map((item, index) => (
               <div key={index} className="flex items-center gap-4">
-                <dt>
-                  <IconImg id={item.src} alt={item.alt} />
+                <dt className="text-text-secondary">
+                  <IconImg id={item.src} alt={item.alt} size={20} />
                 </dt>
                 <dd className="flex items-center gap-4">
                   {item.site ? (
@@ -79,13 +52,13 @@ export default function Introduce() {
                         <React.Fragment key={siteIndex}>
                           <a
                             href={siteItem.href}
-                            className="hover:text-primary flex items-center gap-1 hover:underline"
+                            className="hover:text-primary flex items-center gap-2 hover:underline"
                           >
-                            <StackIcon id={siteItem.icon} size={20} />
+                            <StackIcon id={siteItem.icon} size={16} />
                             {siteItem.label}
                           </a>
                           {siteIndex < item.site.length - 1 && (
-                            <hr className="border-primary h-3 border" />
+                            <hr className="border-border h-3 border" />
                           )}
                         </React.Fragment>
                       ))}
@@ -98,17 +71,7 @@ export default function Introduce() {
             ))}
           </dl>
         </div>
-        <figure className="gsap-fade-in bg-primary/20 flex aspect-video items-center justify-center rounded-3xl p-6 sm:aspect-square sm:w-[300px] sm:min-w-[200px]">
-          <Image
-            src="/images/memoji.png"
-            alt="지인의 미모티콘"
-            width={390}
-            height={422}
-            className="w-1/2 sm:w-2/3"
-            priority
-          />
-        </figure>
-      </div>
-    </section>
+      </SectionLayout>
+    </>
   );
 }
--- src/constants/personal.ts
+++ src/constants/personal.ts
@@ -26,21 +26,3 @@ export const contact = [
     ],
   },
 ];
-
-export const homeAboutMenu = [
-  {
-    label: 'GitHub',
-    href: process.env.NEXT_PUBLIC_GITHUB!,
-    isExternal: true,
-  },
-  {
-    label: 'Velog',
-    href: process.env.NEXT_PUBLIC_VELOG!,
-    isExternal: true,
-  },
-  {
-    label: '이력서 다운로드',
-    href: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/personal//Anjiin_resume.pdf`,
-    download: true,
-  },
-];
