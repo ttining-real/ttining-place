25d8d773d12d4efa9f7dc64c36743f50de823e9b
ttining <ttining.real@gmail.com>
2025-07-09 10:52:21 +0900

    [Feature] 공통 섹션 레이아웃 컴포넌트 생성
    - 공통으로 사용되는 레이아웃용 컴포넌트 생성
    - 스크롤 애니메이션 추가
      - 요소가 뷰포트의 -20% 지점에 들어올 때 opacity, y값 전환

---
+++ src/components/section-layout.tsx
@@ -0,0 +1,46 @@
+import { ReactNode, useRef } from 'react';
+import { motion, useInView, Easing } from 'framer-motion';
+
+type SectionLayoutProps = {
+  outerClassName?: string;
+  innerClassName?: string;
+  children: ReactNode;
+};
+
+export default function SectionLayout({
+  outerClassName = '',
+  innerClassName = '',
+  children,
+}: SectionLayoutProps) {
+  const ref = useRef<HTMLElement>(null);
+
+  const isInView = useInView(ref, {
+    margin: '-20% 0px -20% 0px',
+  });
+
+  const easeOut: Easing = [0.42, 0, 0.58, 1];
+
+  const variants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.7, ease: easeOut },
+    },
+  };
+
+  return (
+    <motion.section
+      ref={ref}
+      aria-label="section layout"
+      className={`px-6 py-20 ${outerClassName}`}
+      variants={variants}
+      initial="hidden"
+      animate={isInView ? 'visible' : 'hidden'}
+    >
+      <div className={`m-auto flex max-w-5xl flex-col gap-8 ${innerClassName}`}>
+        {children}
+      </div>
+    </motion.section>
+  );
+}
