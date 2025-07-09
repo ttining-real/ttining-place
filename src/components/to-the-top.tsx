0ae85a9e12a8b4512683b9fc3ae1216652212655
ttining <ttining.real@gmail.com>
2025-07-09 11:12:57 +0900

    [Refactor] 맨 위로 이동 버튼 컴포넌트 리팩토링
    - 버튼 컴포넌트 적용
    - window.scrollY 값이 100px 이상일 때 보이도록 개선
    - 스크롤 이벤트 리스너 등록 및 해제

--- src/components/to-the-top.tsx
+++ src/components/to-the-top.tsx
@@ -1,6 +1,23 @@
-import Icon from '@/components/icon';
+import { useEffect, useState } from 'react';
+
+import Button from '@/components/button';
 
 export default function ToTheTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibillity = () => {
+      if (window.scrollY > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibillity);
+    return () => window.removeEventListener('scroll', toggleVisibillity);
+  }, []);
+
   const handleTopPosition = () => {
     if (typeof window !== 'undefined') {
       window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -8,12 +25,19 @@ export default function ToTheTop() {
   };
 
   return (
-    <button
-      className="bg-primary/90 border-primary/30 hover:bg-primary focus-ring fixed right-6 bottom-6 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border-2"
-      onClick={handleTopPosition}
-      aria-label="맨 위로 이동"
-    >
-      <Icon id="arrow-top" size={24} className="text-white" />
-    </button>
+    <>
+      <Button
+        variants="primary"
+        isIconOnly
+        iconId="arrow-top"
+        ariaLabel="맨 위로 이동"
+        className={`bg-primary/90 border-primary/30 hover:bg-primary focus-ring fixed right-6 bottom-6 flex h-12 w-12 items-center justify-center rounded-full border-2 transition-opacity duration-300 ${
+          isVisible
+            ? 'pointer-events-auto opacity-100'
+            : 'pointer-events-none opacity-0'
+        }`}
+        onClick={handleTopPosition}
+      />
+    </>
   );
 }
