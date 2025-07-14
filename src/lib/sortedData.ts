85d96b3651ebc077a8fdffed8cc9c47e4a1cd047
ttining <ttining.real@gmail.com>
2025-07-14 14:34:07 +0900

    [Refactor] sortedData 유틸 함수 리팩토링
    - ExperienceDataTypes, ProjectsDataTypes → 제네릭 타입으로 리팩토링
    - 정렬 기준 수정
      - display_order 오름차순
      - display_order 같을 경우, main 우선

--- src/lib/sortedData.ts
+++ src/lib/sortedData.ts
@@ -1,8 +1,9 @@
 import { ExperienceDataTypes } from '@/types/experience-data-type';
-import { ProjectsDataTypes } from '@/types/projects-data-type';
 
 // experience 데이터 정렬
-export function sortedExperienceData(data: ExperienceDataTypes[]) {
+export function sortedExperienceData<T extends ExperienceDataTypes>(
+  data: T[],
+): T[] {
   return [...data].sort(
     (a, b) =>
       new Date(b.start_date).getTime() - new Date(a.start_date).getTime(),
@@ -10,14 +11,20 @@ export function sortedExperienceData(data: ExperienceDataTypes[]) {
 }
 
 // projects 데이터 정렬
-export function sortedProjectsData(data: ProjectsDataTypes[]) {
+export function sortedProjectsData<
+  T extends { type: 'main' | 'side'; display_order: number },
+>(data: T[]): T[] {
   return [...data].sort((a, b) => {
-    // 1. type 기준 정렬: main이 먼저
+    // 1. display_order 기준 오름차순
+    if (a.display_order !== b.display_order) {
+      return a.display_order - b.display_order;
+    }
+
+    // 2. display_order 같으면 type 기준: main 우선
     if (a.type !== b.type) {
       return a.type === 'main' ? -1 : 1;
     }
 
-    // 2. 같은 type이면 display_order 오름차순
-    return a.display_order - b.display_order;
+    return 0;
   });
 }
