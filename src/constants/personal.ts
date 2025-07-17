e2a29b767f9c2bde8a1162158520649bf80d426b
ttining <ttining.real@gmail.com>
2025-07-17 17:33:10 +0900

    [Modify] contact 수정
    - label, ariaLabel 추가

--- src/constants/personal.ts
+++ src/constants/personal.ts
@@ -2,26 +2,31 @@ export const contact = [
   {
     href: process.env.NEXT_PUBLIC_PHONE,
     src: 'telephone',
-    alt: '연락처 아이콘',
+    alt: '전화 아이콘',
+    label: '전화번호',
   },
   {
     href: process.env.NEXT_PUBLIC_EMAIL,
     src: 'e-mail',
     alt: '이메일 아이콘',
+    label: '이메일',
   },
   {
     src: 'web_site',
     alt: '웹 사이트 아이콘',
+    label: '웹사이트',
     site: [
       {
         href: process.env.NEXT_PUBLIC_GITHUB,
         icon: 'github',
         label: 'GitHub',
+        ariaLabel: 'ttining-real GitHub 페이지 열기',
       },
       {
         href: process.env.NEXT_PUBLIC_VELOG,
         icon: 'velog',
         label: 'Velog',
+        ariaLabel: 'ttining-real Velog 블로그 열기',
       },
     ],
   },
